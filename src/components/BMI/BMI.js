41040d10c1111212e13414c98f19bc43c82e8ea3
seoyeon <tjsus247@naver.com>
2022-09-21 19:24:54 +0900

    BMI추가

--- src/components/BMI/BMI.js
+++ src/components/BMI/BMI.js
@@ -4,7 +4,7 @@ import './BMI.css'
 const BMI = (props) =>{
     return <div className="BMIpage">
         <div className="cal">
-            
+            <p>BMI 계산기</p>
         </div>
     </div>
 }
