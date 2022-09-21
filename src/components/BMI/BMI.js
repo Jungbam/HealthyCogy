99e39cec9a19da04d80194c9b0d6fd2c195de856
seoyeon <tjsus247@naver.com>
2022-09-21 14:29:37 +0900

    수정

---
+++ src/components/BMI/BMI.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './BMI.css'
+
+const BMI = (props) =>{
+    return <div className="BMIpage">
+        <div className="cal">
+            
+        </div>
+    </div>
+}
+export default BMI
