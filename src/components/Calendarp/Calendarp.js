d19a3c2596db8112f6429026e36c75af6b01fbd1
seoyeon <tjsus247@naver.com>
2022-09-28 09:43:37 +0900

    Calendar 추가

---
+++ src/components/Calendarp/Calendarp.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'; // css import
+import './Calendarp.css'
+import moment from 'moment';
+
+
+function Calendarp() {
+    const DayBtn = document.getElementsByClassName('react-calendar__tile react-calendar__month-view__days__day')
+    
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <div className='Calendar'>
+      <Calendar className="r" onChange={onChange} value={value} />
+      <div className="text-gray-500 mt-4">
+           {moment(value).format("YYYY년 MM월 DD일")} 
+         </div>
+    </div>
+  );
+}
+export default Calendarp
---
+++ src/components/MyStroage/MyStroage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Calendarp from "../Calendarp/Calendarp";
+import './MyStroage.css'
+
+const MyStroage = () =>{
+    return<div className="MyStorage">
+    <Calendarp/>
+</div>
+}
+export default MyStroage;
