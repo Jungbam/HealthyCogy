f3676d4193b6caa3539634075baa0341dd296913
Jungbam <108196588+Jungbam@users.noreply.github.com>
2022-10-02 21:55:45 +0900

    추천 영상 작업 완료

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
 import Router from './components/Router'
-import WeekRoutin from './components/WeekRoutin/WeekRoutin'
+import RoutinVideo from './components/WeekContainer/RoutinVideo/RoutinVideo'
+
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 function App() {
   return (
     <div className="App">
-     
-      <Router />
+      <RoutinVideo />
+      {/* <Router /> */}
     </div>
   )
 }
