df2c180912dd649acc015ccda17968e92f2457a0
Jungbam <108196588+Jungbam@users.noreply.github.com>
2022-10-05 15:41:47 +0900

    일주일 비교사진 기능 추가

--- src/components/BoardContainer/ImgBoard/Mypicture/MyPicture.js
+++ src/components/BoardContainer/ImgBoard/Mypicture/MyPicture.js
@@ -7,32 +7,54 @@ const MyPicture = ({ userObj, date }) => {
   const dateId = dayjs(date).format('YY-MM-DD')
   const [videoArray, setVideoArray] = useState([])
   const [data, setData] = useState([])
-  // const [routinValue, setRoutinValue] = useState('')
-
+  const [beforeWeekData, setBeforeWeekData] = useState([])
+  const dayjsDateId = dayjs(date)
+  const beforedateEl = dayjsDateId.add(-7, 'day')
+  console.log(beforedateEl)
   useEffect(() => {
     //실시간으로 DB에서 받아오기.
     dbService.collection('image').onSnapshot((snapshot) => {
       const dataArray = snapshot.docs.map((doc) => ({
         ...doc.data(),
       }))
+      console.log(dataArray)
       const selectedUserArray = dataArray.filter((data) => {
         return data.user === userId
       })
       const outputArray = selectedUserArray.filter((data) => {
         return data.date == dateId
       })
-      const resultArray = outputArray.map((data) => data.url)
-      setData(resultArray)
+      const beforedate = beforedateEl.format('YY-MM-DD')
+      const beforeweekArray = selectedUserArray.filter((data) => {
+        return data.date == beforedate
+      })
+      const resultData = outputArray.map((data) => data.url)
+      setData(resultData)
+      const beforeWeekPictureData = beforeweekArray.map((data) => data.url)
+      setBeforeWeekData(beforeWeekPictureData)
     })
   }, [userId, dateId])
-
+  console.log(data)
+  console.log(beforeWeekData)
+  console.log(data.length)
   return (
-    <div>
-      {data.map((url, index) => (
-        <div className="imgBox" key={index}>
-          <img className="mypicture" src={url} />
+    <div className="mypictureContainer">
+      {beforeWeekData.length === 1 ? (
+        <div className="imgBox">
+          <h2>일주일 전 내모습</h2>
+          <img className="mypicture" src={beforeWeekData} />
         </div>
-      ))}
+      ) : (
+        <h2>일주일 전 내 모습이 없어요.</h2>
+      )}
+      {data.length === 1 ? (
+        <div className="imgBox">
+          <h2>오늘의 내모습</h2>
+          <img className="mypicture" src={data} />
+        </div>
+      ) : (
+        <h2>오늘의 당신의 뱃살을 올려주세요.</h2>
+      )}
     </div>
   )
 }
