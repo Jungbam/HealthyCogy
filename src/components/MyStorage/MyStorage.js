57da943a6ceda75dc3c8f469d0ec1a9b9d577da9
Jungbam <108196588+Jungbam@users.noreply.github.com>
2022-10-05 23:49:13 +0900

    캘린더컨테이너 인풋 수정 / WeekRoutin 코드 수정

--- src/components/MyStorage/MyStorage.js
+++ src/components/MyStorage/MyStorage.js
@@ -8,45 +8,25 @@ import './MyStorage.css'
 
 const MyStorage = (props) => {
   const [date, setDate] = useState(new Date())
-  const [init, setInit] = useState(false)
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [userObj, setUserObj] = useState('')
 
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true)
         setUserObj(user.uid)
       } else {
-        setIsLoggedIn(false)
+        alert('로그인이 필요합니다. 메인페이지로 가세요.')
       }
-      setInit(true)
     })
   }, [])
 
   return (
     <div className="MyStorage">
-      <CalendarContainer
-        date={date}
-        init={init}
-        isLoggedIn={isLoggedIn}
-        userObj={userObj}
-        setDate={setDate}
-      />
+      <CalendarContainer date={date} userObj={userObj} setDate={setDate} />
       {/* 달력 코드 */}
-      <WeekContainer
-        date={date}
-        init={init}
-        isLoggedIn={isLoggedIn}
-        userObj={userObj}
-      />
+      <WeekContainer date={date} userObj={userObj} />
       {/* 운동 루틴 추천, 하루 식단  */}
-      <BoardContainer
-        date={date}
-        init={init}
-        isLoggedIn={isLoggedIn}
-        userObj={userObj}
-      />
+      <BoardContainer date={date} userObj={userObj} />
       {/* 게시판, 영상 알고리즘 */}
     </div>
   )
--- src/components/WeekRoutin/WeekRoutin/WeekRoutin.js
+++ src/components/WeekRoutin/WeekRoutin/WeekRoutin.js
@@ -3,7 +3,7 @@ import dayjs from 'dayjs'
 import './WeekRoutin.css'
 import GetWeekRoutinList from '../GetWeekRoutinList/GetWeekRoutinList'
 
-const WeekRoutin = ({ dayValue, userObj, setPage, shutDownHandler }) => {
+const WeekRoutin = ({ dayValue, userObj }) => {
   const dayCalculationArray = [] //클릭한 날짜 일주일간 배열 생성
   const CalculationNum = dayValue.getDay() //클릭한 날짜 인덱스 배열로 변환코드
   let startdayValue = dayjs(dayValue)
@@ -18,12 +18,7 @@ const WeekRoutin = ({ dayValue, userObj, setPage, shutDownHandler }) => {
     <div className="WeekRoutin">
       {dayCalculationArray.map((date) => (
         <div key={Math.random()}>
-          <GetWeekRoutinList
-            userObj={userObj}
-            date={date}
-            setPage={setPage}
-            shutDownHandler={shutDownHandler}
-          />
+          <GetWeekRoutinList userObj={userObj} date={date} />
         </div>
       ))}
     </div>
