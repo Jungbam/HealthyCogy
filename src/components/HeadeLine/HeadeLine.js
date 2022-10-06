e2e9d9db1e2e01a2b0cee816bdda9c5d3fd002cf
seoyeon <tjsus247@naver.com>
2022-10-06 17:23:11 +0900

    햄버거 스타일링중2

--- src/components/HeadeLine/HeadeLine.js
+++ src/components/HeadeLine/HeadeLine.js
@@ -13,13 +13,41 @@ const HeadeLine = (props) => {
         <h1 id="page_logo">헬시코기</h1>
       </NavLink>
       <nav className="navItems">
-        <p>{props.user.displayName}님 반갑습니다.</p> 
+        <p>{props.user.displayName}님 반갑습니다.</p>
+        <div className="header1">
+          <ul>
+            <li>
+              <NavLink
+                style={{ textDecoration: 'none', color: 'white' }}
+                to="/bmi"
+              >
+                BMI계산기
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                style={{ textDecoration: 'none', color: 'white' }}
+                to="/food"
+              >
+                식단추천
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                style={{ textDecoration: 'none', color: 'white' }}
+                to="/Mystorage"
+              >
+                MyStorage
+              </NavLink>
+            </li>
+          </ul>
+        </div> 
         <img className="userProfile" src={props.user.photoURL}></img>
-        <NavLink to="/" style={{ textDecoration: 'none' }}>
-          <button className="logoutBtn" onClick={logoutHandler}>
-            로그아웃
-          </button>
-        </NavLink>   
+            <NavLink to="/" style={{ textDecoration: 'none' }}>
+              <button className="logoutBtn" onClick={logoutHandler}>
+                로그아웃
+              </button>
+            </NavLink>
         <input type="checkbox" id="icon" />
         <label htmlFor="icon">
           <span></span>
