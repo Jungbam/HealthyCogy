9f329f9513a5aa77e606aa9f06951733df5cc87d
Jungbam <108196588+Jungbam@users.noreply.github.com>
2022-10-07 21:34:51 +0900

    도움말 작업 완료

--- src/components/BoardContainer/ImgBoard/ImgBoard.js
+++ src/components/BoardContainer/ImgBoard/ImgBoard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import TipWindow from '../../TipWindow/TipWindow'
 import './ImgBoard.css'
 import MyDietProcess from './MyDietProcess/MyDietProcess'
 import MyPicture from './Mypicture/MyPicture'
@@ -6,6 +7,13 @@ import MyPicture from './Mypicture/MyPicture'
 const ImgBoard = (props) => {
   return (
     <div className="ImgBoard">
+      <TipWindow>
+        <h2>변해가는 나,</h2>
+        <h2> 기록하기</h2>
+        <hr></hr>
+        <p>하루하루 변해가는 나를 기록해봐요.</p>
+        <p>일주일 전과는 많이 변하셨나요?</p>
+      </TipWindow>
       <MyPicture userObj={props.userObj} date={props.date} />
       <MyDietProcess userObj={props.userObj} date={props.date} />
     </div>
